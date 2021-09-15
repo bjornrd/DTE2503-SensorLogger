4dd2fd3a1124303c8093fe05b1b8048dc7cf352f
Bjørn Rudi Dahl <bjorn.rudi.dahl@outlook.com>
2021-09-15 20:46:42 +0200

    defaulting to all sensors and sensor types ON

--- app/src/main/java/com/bear/sensordatalogger/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/bear/sensordatalogger/ui/dashboard/DashboardViewModel.java
@@ -2,6 +2,9 @@ package com.bear.sensordatalogger.ui.dashboard;
 
 import androidx.lifecycle.ViewModel;
 
+
+// Share data between fragments:
+// https://developer.android.com/guide/fragments/communicate#fragments
 public class DashboardViewModel extends ViewModel {
     public DashboardViewModel()
     {
